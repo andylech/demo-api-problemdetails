ae67cefcaadbbd03af0eabe817f8f9c110c0f04f
Andy Lech <andylech@users.noreply.github.com>
2024-10-21 12:16:00 -0400

    Add future tester for bundling PDs out of hard-to-replicate status codes

---
+++ Futurama/Shared/Testing/DevTesting.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Net;
+using static ProblemDetailsApiDemo.Futurama.Shared.ProblemDetails.ProblemBundler;
+using static System.Int32;
+
+namespace ProblemDetailsApiDemo.Futurama.Shared.Testing
+{
+    internal static class DevTesting
+    {
+        // For testing of response to codes which are hard to set up or re-create
+        internal static IResult? TryNumberAsStatusCode(object value)
+        {
+            int? statusCode = value switch
+            {
+                int num => num,
+                string str when TryParse(str, out var strInt) => strInt,
+                _ => null
+            };
+
+            if (statusCode is null
+                || !Enum.IsDefined(typeof(HttpStatusCode), statusCode))
+                return null;
+
+            var problemDetails =
+                BundleProblemDetails((HttpStatusCode)statusCode);
+
+            return Results.Problem(problemDetails);
+        }
+    }
+}
